74ebae30c45b6e3d8536fb36d0823aa7305eb0b5
tada <rortheford@gmail.com>
2022-12-19 15:49:38 -0800

    Add button to change grid size

--- sketch.js
+++ sketch.js
@@ -1,36 +1,55 @@
-const GRID_ROWS = 16;
-const GRID_COLS = 16;
-
-function createGrid() {
+function createGrid(gridsize) {
     let cell;
-    let gridContainer = document.querySelector('.container');
     gridContainer.style.display = 'inline-grid';
     gridContainer.style.width = '800px';
     gridContainer.style.height = '800px';
+    //gridConatiner.style.margin = '0px';
 
-    for (let i = 0; i < GRID_ROWS; i++) {
+    for (let i = 0; i < parseInt(gridsize); i++) {
         gridContainer.style.gridTemplateColumns += ' auto';
-        for (let j = 0; j < GRID_COLS; j++) {
+        for (let j = 0; j < parseInt(gridsize); j++) {
             cell = document.createElement('div');
             cell.setAttribute('id', 'grid-cell');
             gridContainer.appendChild(cell);
         }
     }
-    console.log(gridContainer.style.gridTemplateColumns);
 
     let cellStyle = document.querySelectorAll('#grid-cell');
     cellStyle.forEach ((cell) => {
-        cell.style.border = '1px solid black';
+        cell.style.border = '0.5px solid black';
         cell.style.textAlign = 'center';
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = 'black';
+        })
     });
 }
 
-document.querySelector('body').style.textAlign = 'center';
-createGrid();
+function gridSize() {
+    let size = prompt("What size should the new grid be?");
+    while (size > 100) {
+        alert("The maximum grid size is 100.");
+        size = prompt("What size should the new grid be?");
+    }
+    return size;
+}
 
-let cellAll = document.querySelectorAll('#grid-cell');
-cellAll.forEach ((cell) => {
-    cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = 'black';
-    })
+let body = document.querySelector('body');
+body.style.textAlign = 'center';
+
+let gridContainer = document.querySelector('.container');
+
+let sizeBtn = document.createElement('button');
+sizeBtn.style.padding = '15px';
+sizeBtn.style.margin = '0px 30px';
+sizeBtn.textContent = "Change Grid Size";
+body.appendChild(sizeBtn);
+
+createGrid(16);
+sizeBtn.addEventListener('click', () => {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+    gridContainer.style.gridTemplateColumns = '';
+    let size = gridSize();
+    createGrid(size);
 })
